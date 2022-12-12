dcea2d3ec04b90716dffff3cda97b6f610ba0e33
kenlaxar <kenlaxar@gmail.com>
2022-12-12 00:32:33 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,11 +9,11 @@ int main(void)
 	int c, d;
 
 	for (c = '0'; c <= '9'; c++)
-		for (d = c + 1; d <= 10;)
+		for (d = c + 1; d <= 10; d++)
 		{
-			putchar(c);
-			putchar(d);
-			if (!(c == 8) || (d == 9))
+			putchar((c % 10) + 0);
+			putchar((d % 10) + 0);
+			if (c == 8 && d == 9)
 			{
 				putchar(',');
 				putchar(' ');
