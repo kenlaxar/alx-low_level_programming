b922091dff131910903e5c529a9c6151f83aee3e
kenlaxar <kenlaxar@gmail.com>
2022-12-11 21:28:14 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry pont
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	int d;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (d = '0'; d <= '9'; d++)
+		{
+			if ((d != c) || (c < d))
+			{
+				putchar(c);
+				putchar(d);
+				if (!(d == '9' && c == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
