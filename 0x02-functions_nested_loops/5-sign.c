1fed75e777fe29166895a30a77c4d4a8af1130b3
kenlaxar <kenlaxar@gmail.com>
2022-12-14 19:44:45 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of number
+ * @n: is number
+ * Return: 1 if greater than zero, 0 if zero, -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
