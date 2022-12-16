789846756c38d420b5bbf8c4dfd4bfcf2b32de95
kenlaxar <kenlaxar@gmail.com>
2022-12-15 22:11:30 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,21 +3,18 @@
 /**
  * more_numbers - prints more numbers
  */
-
 void more_numbers(void)
 {
 	int i, j;
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar('i');
-				_putchar(i % 10 + '0');
-			}
+				_putchar('1');
+			_putchar(i % 10 + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,19 +4,21 @@
  * main - prints buzz each numbers of 3 and 5
  * Return: Always 0
  */
+
 int main(void)
 {
 	int n;
 
 	n = 1;
 	printf("%d", n);
+
 	for (n = 2; n <= 100; n++)
 	{
 		for ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
-		else if (n % 3 == 0)
+		else (n % 3 == 0)
 		{
 			printf(" Fizz");
 		}
