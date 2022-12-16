02a8e5bc7ba73fe6cd50e13b74bfdb530eb92f60
kenlaxar <kenlaxar@gmail.com>
2022-12-16 04:22:50 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,11 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 1; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar(i % 10 + '0');
+			_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
