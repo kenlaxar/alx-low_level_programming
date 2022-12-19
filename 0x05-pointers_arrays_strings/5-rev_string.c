7c427467660fde047f7660894661625c33d9531c
kenlaxar <kenlaxar@gmail.com>
2022-12-18 22:50:07 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses the string
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j, k, tmp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		tmp = s[k];
+		s[k] = s[j];
+		s[j] = tmp;
+		k++;
+		j--;
+	}
+}
